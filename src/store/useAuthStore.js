3414561d0196b03a471a0ccd928fe23ccbaacf58
Satrio Prabowo <satrioprabowo47@gmail.com>
2026-04-01 23:23:20 +0700

    feat: use localStorage for persist data

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -1,14 +1,21 @@
 import { create } from "zustand";
 
 const useAuthStore = create((set) => ({
-  user: null,
-  token: null,
-  login: (data) => set({ user: data, token: data.accessToken }),
-  logout: () =>
+  user: JSON.parse(localStorage.getItem("user")) || null,
+  token: localStorage.getItem("token") || null,
+  login: (data) => {
+    localStorage.setItem("user", JSON.stringify(data));
+    localStorage.setItem("token", data.accessToken);
+    set({ user: data, token: data.accessToken });
+  },
+  logout: () => {
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
     set({
       user: null,
       token: null,
-    }),
+    });
+  },
 }));
 
-export default useAuthStore
+export default useAuthStore;
